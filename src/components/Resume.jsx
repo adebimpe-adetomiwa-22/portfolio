37d209a0a3d43efbde24f879a2400736e9934dc7
Adebimpe Adetomiwa <adebimpeadetomiwa22@gmail.com>
2024-08-21 06:51:17 -0700

    add resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ciscoCert from '../assets/certs/Data_Analytics_Essentials_Badge20240519-7-ppl4lw.pdf';
 import ciscoCertImage from '../assets/certs/Data_Analytics_Essentials_Badge20240519-7-ppl4lw.jpg';
+import resumePdf from '../assets/resume/Adebimpe_Adetomiwa_resume.pdf';
+import resumeImage from '../assets/resume/Adebimpe_Adetomiwa_resume.png';
 // import DownloadForOfflineRoundedIcon from '@mui/icons-material/DownloadForOfflineRounded';
 import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 import { IconButton } from '@mui/material';
@@ -12,18 +14,16 @@ const Resume = () => {
                 <h3 className='resume-title subtitle'>Resume / cv</h3>
                 <div className='certs-wrapper'>
                     <div className='cert'>
-                        <p className='cert-title body'>
-                            Cisco data analysis certificate
-                        </p>
+                        <p className='cert-title body'>My Resume</p>
                         <img
-                            src={ciscoCertImage}
+                            src={resumeImage}
                             alt='cert image'
                             title='cert image'
-                            className='cert-image'
+                            className='cert-image object-top'
                         />
 
                         <a
-                            href={ciscoCert}
+                            href={resumePdf}
                             download={true}
                             className='download-cert'
                         >
@@ -32,9 +32,10 @@ const Resume = () => {
                             </IconButton>
                         </a>
                     </div>
-
                     <div className='cert'>
-                        <p className='cert-title body'>My Resume</p>
+                        <p className='cert-title body'>
+                            Cisco data analysis certificate
+                        </p>
                         <img
                             src={ciscoCertImage}
                             alt='cert image'
